6b2590e06034c8c12cd11b9fb893994f7cf34ecf
Алексей Бельченков <u608110@gmail.com>
2024-05-03 20:44:45 +0400

    make title component

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, ImageBackground } from 'react-native';
+import { StyleSheet, ImageBackground, SafeAreaView, } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useState } from 'react';
 
@@ -32,7 +32,9 @@ export default function App() {
             style={styles.rootScreen}
             imageStyle={styles.backgroundImage}
         >
-            {screen}
+            <SafeAreaView style={styles.rootScreen}>
+                {screen}
+            </SafeAreaView>
         </ImageBackground>
       </LinearGradient>
     );
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,29 @@
-import { Text } from 'react-native';
+import {
+    StyleSheet,
+    Text,
+    View,
+} from 'react-native';
+
+import Title from '../components/Title';
 
 const GameScreen = () => {
     return (
-        <Text>Game Screen!</Text>
+        <View style={styles.screen}>
+            <Title>Opponent's Guess</Title>
+            <Text>GUESS</Text>
+            <View>
+                <Text>Higher or lower?</Text>
+            </View>
+            {/*<View>LOG ROUNDS</View>*/}
+        </View>
     );
 };
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 24,
+    },
+});
