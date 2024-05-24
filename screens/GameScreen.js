86d8de0e669bfa54de3e7a6dfe530df5b1ced5a9
Алексей Бельченков <u608110@gmail.com>
2024-05-24 17:49:47 +0400

    add game control buttons

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -3,10 +3,15 @@ import {
     StyleSheet,
     Text,
     View,
+    Alert,
 } from 'react-native';
 
 import Title from '../components/ui/Title';
 import NumberContainer from '../components/game/NumberContainer';
+import PrimaryButton from '../components/ui/PrimaryButton';
+
+let minBoundary = 1;
+let maxBoundary = 100;
 
 function generateRandomBetween(min, max, exclude) {
     const rndNum = Math.floor(Math.random() * (max - min)) + min;
@@ -15,8 +20,27 @@ function generateRandomBetween(min, max, exclude) {
 }
 
 const GameScreen = ({ userNumber }) => {
-    const initialGuess = generateRandomBetween(1, 100, userNumber);
+    const initialGuess = generateRandomBetween(minBoundary, maxBoundary, userNumber);
     const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+    const nextGuessHandler = direction => {
+        if ((direction === 'lower' && currentGuess < userNumber) || (direction === 'greater' && currentGuess > userNumber)) {
+            Alert.alert("Don't lie!", 'You know that this is wrong...', {
+                text: 'Sorry!',
+                style: 'cancel',
+            });
+            return;
+        }
+
+        if (direction === 'lower') {
+            maxBoundary = currentGuess;
+        } else {
+            minBoundary = currentGuess + 1;
+        }
+
+        const newRndNumber = generateRandomBetween(minBoundary, maxBoundary, currentGuess);
+        setCurrentGuess(newRndNumber);
+    };
 
     return (
         <View style={styles.screen}>
@@ -24,6 +48,10 @@ const GameScreen = ({ userNumber }) => {
             <NumberContainer>{currentGuess}</NumberContainer>
             <View>
                 <Text>Higher or lower?</Text>
+                <View>
+                    <PrimaryButton onPress={() => nextGuessHandler('lower')}>-</PrimaryButton>
+                    <PrimaryButton onPress={() => nextGuessHandler('greater')}>+</PrimaryButton>
+                </View>
             </View>
             {/*<View>LOG ROUNDS</View>*/}
         </View>
